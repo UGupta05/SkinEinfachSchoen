5017dd3b3075558c600fdc1e5654bb48dc92b727
Utkarsh Gupta <utkarsh.gupta4@gmail.com>
2026-05-24 19:39:50 +0200

    ci: run supabase link in CI runner to set up IPv4 connection

--- supabase/migrations/20260524000000_init_schema.sql
+++ supabase/migrations/20260524000000_init_schema.sql
@@ -1,4 +1,4 @@
--- Initial database schema for Skin Einfach Schön (Triggering Test Run v4)
+-- Initial database schema for Skin Einfach Schön (Triggering Test Run v5)
 
 -- Create appointments table
 CREATE TABLE IF NOT EXISTS appointments (
